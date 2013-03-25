bdaa55564d3bc87bf983a18a9ba033a6e89eaf5c
Alex Shvets <github@sashok.mk.ua>
2013-03-25 18:14:22 +0200

    version 0.7.1
    change namespace functionality

--- src/clone.js
+++ src/clone.js
@@ -1,7 +1,7 @@
 'use strict';
 /**
  * @title   clone.js - the true prototype-based JavaScript micro-framework.
- * @version 0.7.0 beta
+ * @version 0.7.1 beta
  * @author  Alex Shvets
  *
  * @class
