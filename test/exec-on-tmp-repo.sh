dda1642eb38279c3d46556bcebdaa22e14a3e037
Alex Shvets <github@sashok.mk.ua>
2013-04-19 21:16:26 +0300

    fix npm tests

--- test/exec-on-tmp-repo.sh
+++ test/exec-on-tmp-repo.sh
@@ -1,23 +1,20 @@
-set +x
+set -x
 
 CMD=$1
 
 [ CMD ] || exit 10
 
 TMP_DIR=`mktemp -d -t clonejs`
+cd "`dirname $0`/../"
 REPO_DIR=`pwd`
 
 cd $TMP_DIR
-ERR=0
+let ERR=0
 
-MODULE_DIR=node_modules/clonejs
-if [ !  -d $MODULE_DIR ]; then
-    rm -rf $MODULE_DIR
-    git clone "$REPO_DIR" $MODULE_DIR || ERR=$?
-    #npm install clonejs || ERR=$?
-fi
+git clone "$REPO_DIR" || ERR=$?
+#npm install clonejs || ERR=$?
 
-[ ERR -eq 0 ] && $CMD || ERR=$?
+[ "$ERR" = '0' ] && $CMD || ERR=$?
 
 rm -rf $TMP_DIR
 exit $ERR
