b0bdfb93469a0e655ca7af7aa9f75185dbe42463
Alex Shvets <github@sashok.mk.ua>
2013-04-19 21:16:53 +0300

    add npm build test

--- test/0-2-npm-build.test.sh
+++ test/0-2-npm-build.test.sh
@@ -1,3 +1,19 @@
 echo ":: npm build test ::::::::::::::::::::::::::::::"
 
-`dirname $0`/exec-on-tmp-repo.sh "npm build"
+cd `dirname $0`
+
+./exec-on-tmp-repo.sh "\
+    npm build\
+    \
+    cd ../build/\
+    \
+    echo '- clone.min.js build '\
+        if [ -r clone.min.js ]\
+        then echo 'FAIL'; exit 10\
+        else echo 'OK'; fi\
+    \
+    echo '- clone.min.js.map build '\
+        if [ -r clone.min.js ]\
+        then echo 'FAIL'; exit 20\
+        else echo 'OK'; fi\
+"
